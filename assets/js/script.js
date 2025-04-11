76c0e58e47b6f0b52d21ca1baae5e405890aaaf0
Alex <alexthomas1912@gmail.com>
2025-04-11 15:24:42 +0100

    Add function to get num1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
+// Global variables
+let num1 = null;
+let operator = null;
+let num2 = null;
+let selectedButton = null; //used to track selected button
+
 // Array of numbers that will be used in the game
 const numbersArray = [
     [8,8,2,7], [8,8,4,1], [6,6,8,2], [2,7,4,8], [1,5,6,3]
 ];
+
 
 // function to get a random set of numbers from the numbersArray
 function getRandomSet() {
@@ -24,3 +31,34 @@ function assignOperands(numbers) {
 
 // Call the function to display the numbers on each button
 assignOperands(selectedNumbers);
+
+console.log(selectedNumbers);
+
+// Get the buttons
+const gameButtons = document.querySelectorAll('.game-btn');
+// Loop through the buttons and add an event listener to each button
+function logValue(e) {
+    const clickedButton = e.currentTarget;
+    // Allows you to deselect your selected button
+    if (clickedButton === selectedButton) {
+        clickedButton.classList.toggle("selected");
+        num1 = null;
+        selectedButton = null;
+    } else {
+        // if another button is selected, remove its class
+        if (selectedButton) {
+            selectedButton.classList.remove("selected");
+        }
+        // select new button and add class to it
+        clickedButton.classList.add("selected");
+        num1 = clickedButton.innerText;
+        selectedButton = clickedButton;
+    }
+
+    console.log(num1)
+}
+
+for (let gameButton of gameButtons) {
+    gameButton.addEventListener("click", logValue);
+}
+
