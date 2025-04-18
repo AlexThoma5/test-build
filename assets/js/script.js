aa1b86a40a334fd932b0ee0a3d14561e02dfd5ac
Alex <alexthomas1912@gmail.com>
2025-04-18 02:17:55 +0100

    new build

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,97 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons) {
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "start") {
-                alert("You clicked start game!");
+                startGame();
+            } else if (this.getElementsByClassName("game-btn")){
+                handleNumberClick(event);
             } else {
-                let buttonType = this.getAttribute("data-type")
-                alert(`You clicked ${buttonType}`)
+                alert(`You clicked ${data-type}`);
             }
         })
     }
+
 })
 
+/** The main game "loop", called when the game is first loaded
+ * and after the users answer has been processed
+ */
 function startGame() {
 
+    let num1 = null;
+    let operator = null;
+    let num2 = null;
+
+    displayNumbers();
+
+}
+
+function getRandomSet() {
+
+    // Array of numbers that will be used in the game
+    const numbersArray = [[8,8,2,7], [8,8,4,1], [6,6,8,2], [2,7,4,8], [1,5,6,3]];
+
+    // Creates random number between 0 and the array length
+    const randomNum = Math.floor(Math.random() * numbersArray.length);
+        
+    // Stores random array from numbers array into variable
+    const randomSet = numbersArray[randomNum];
+    
+    return randomSet;
 }
 
 function displayNumbers() {
+    
+    // Stores the randomSet from the numbers array into a variable
+    const selectedNums = getRandomSet();
+    
+    // Inserts the numbers from the selected array into each button
+    document.getElementById('operand1').innerText = selectedNums[0];
+    document.getElementById('operand2').innerText = selectedNums[1];
+    document.getElementById('operand3').innerText = selectedNums[2];
+    document.getElementById('operand4').innerText = selectedNums[3];
 
 }
+
+// Variables needed for handle number function
+let lastClickedButton = null;
+let step = 1;
+let chosenNumber = null;
 
 function handleNumberClick(event) {
 
+    // stores clicked button into a variable
+    const clickedButton = event.currentTarget;
+    
+    // Deselect if same button clicked
+    if (clickedButton === lastClickedButton) {
+        clickedButton.classList.toggle("selected");
+        chosenNumber = null;
+        lastClickedButton = null;
+    } else {
+        // Deselect previous button if any
+        if (lastClickedButton) {
+            lastClickedButton.classList.remove("selected"); 
+        }
+        // Select new button
+        clickedButton.classList.add("selected");
+        lastClickedButton = clickedButton;
+        chosenNumber = clickedButton.innerText;
+    }
+
+    if (step === 1) {
+        num1 = chosenNumber;
+    } else if (step === 3) {
+        num2 = chosenNumber;
+    }
+
+    console.log(`num1: ${num1}`);
+    console.log(`num2: ${num2}`);
 }
 
 function handleOperatorClick(event) {
+    if (step === 2) {
 
+    }
 }
 
 function performCalculation(num1, operator, num2) {
